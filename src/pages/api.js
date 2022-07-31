170f7ff635927006be8fb1561cdf49d122c43573
Hamza Malik <37263285+hamza7malik@users.noreply.github.com>
2022-07-31 12:00:19 -0700

    Hamza Commot

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,65 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Formik,useFormik } from 'formik';
+import { Formik, useFormik } from 'formik';
 
 const Api = () => {
-
-  const formik = useFormik({
-    initialValues: "",
-    onSubmit: handleSubmit,
-    
+  const [std, setStd] = useState({
+    name: 'Anas',
+    age: '27',
+    gender: 'sheMale',
   });
-  const [age, setAge]=useState  ({
-    name:"",
-    gender:'',
-    count:'',
-    ip:'',
-  }) ;
-  useEffect(()=>{
-    axios.get(`https://api.ipify.org?format=json`).then((res)=>{
+  const initialValues = {
+    name: '',
+  };
+  const formik = useFormik({
+    initialValues: initialValues,
+    onSubmit: handleSubmit,
+  });
+  const [age, setAge] = useState({
+    name: '',
+    gender: '',
+    count: '',
+  });
+  const [ip, setIp] = useState('');
+  useEffect(() => {
+    // ------------------Other api call-------------
+    axios.get(`https://api.genderize.io?name=Anas`).then((res) => {
+      console.log(res);
       setAge({
-        ip:res.data,
-      })
-    })
-    
-       
-      },[]);
-    
-  function handleSubmit(values) {
-    console.log(values);
-    axios.get(`https://api.genderize.io?name=${values}`).then((res) => {
-      
-      setAge({
-        name:res.data,
-        gender:res.data,
-        count:res.data,
+        name: res.data.name,
+        gender: res.data.gender,
+        count: res.data.count,
       });
-    
-      
-     },[]);
-    
-  };
-    
+    });
+    axios.get(`https://api.ipify.org?format=json`).then((res) => {
+      setIp(res.data.ip);
+    });
+  }, []);
+
+  function handleSubmit(values) {
+    setStd({ ...std, name: 'Hamza', gender: 'Male' });
+    console.log(`STD: ${std.name} ${std.age} ${std.gender}`);
+    console.log(values.name);
+    axios.get(`https://api.genderize.io?name=${values.name}`).then((res) => {
+      setAge({
+        name: res.data.name,
+        gender: res.data.gender,
+        count: res.data.count,
+      });
+    });
+  }
+
   return (
-    
     <div>
-      <p>{`${age.ip}`}</p>
-      <input
-              type='text'
-               name='name'
-              placeholder='Name'
-              
-            />
-       
-        <button onClick={handleSubmit}>Click Me</button>
-        <p>{`${age.name}`}</p>
-        <p>{`${age.gender}`}</p>
-        <p>{`${age.count}`}</p>
-      
+      <p>{`${ip}`}</p>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name'
+          onChange={formik.handleChange}
+        />
+        <button type='submit'>Click Me</button>
+      </form>
+      <p>{`${age.name}`}</p>
+      <p>{`${age.gender}`}</p>
+      <p>{`${age.count}`}</p>
     </div>
-  )
-  };
+  );
+};
 
-
-export default Api
-  
+export default Api;
